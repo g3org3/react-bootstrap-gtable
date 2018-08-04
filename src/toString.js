a0d601e390dcfe975bf2d38a6496b2afa9c5a14d
George <7jagjag@gmail.com>
2018-08-04 02:31:58 +0200

    enhancement , render custom dom

--- src/toString.js
+++ src/toString.js
@@ -2,6 +2,15 @@ export default (item, {showObject, defaultStringForObject} = {}) => {
   if (typeof item !== 'object') {
     return `${item}`
   }
+
+  if (!item) {
+    return ''
+  }
+
+  if (item.__dom) {
+    return item.__dom
+  }
+
   if (showObject) {
     return JSON.stringify(item)
   }
